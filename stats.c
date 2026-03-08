9fc2b6354cc67f7feef082fa06dcb4942907d98b
Katya-vo <01205792@pw.edu.pl>
2026-03-08 13:06:43 +0200

    Update stats.c

---
+++ stats.c
@@ -0,0 +1,16 @@
+#include "stats.h"
+
+int minimum(int *arr, int n)
+{
+    return min;
+}
+
+int maximum(int *arr, int n)
+{
+   return max;
+}
+
+double average(int *arr, int n)
+{
+   return (double)sum / n;
+}
