4e0b4ad8eacc06c38786fd79e2d623595b73919f
jagnaszapiel <01206827@pw.edu.pl>
2026-03-08 20:15:07 +0100

    Update stats.c

--- stats.c
+++ stats.c
@@ -2,15 +2,36 @@
 
 int minimum(int *arr, int n)
 {
+    int min = arr[0];
+
+    for (int i = 1; i < n; i++)
+    {
+        if (arr[i] < min)
+            min = arr[i];
+    }
     return min;
 }
 
 int maximum(int *arr, int n)
 {
+    int max = arr[0];
+
+    for (int i = 1; i < n; i++)
+    {
+        if (arr[i] > max)
+            max = arr[i];
+    }
    return max;
 }
 
 double average(int *arr, int n)
 {
+     int sum = 0;
+
+    for (int i = 0; i < n; i++)
+    {
+        sum += arr[i];
+    }
    return (double)sum / n;
 }
+
