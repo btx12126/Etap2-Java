5f64713793a03cb166d4613dc2ee2539c5272cca
jagnaszapiel <01206827@pw.edu.pl>
2026-03-09 01:04:50 +0100

    Update graph.c

--- graph.c
+++ graph.c
@@ -4,5 +4,27 @@
 
 int read_graph(const char *filename, Edge **edges)
 {
+    FILE *f = fopen(filename, "r");
+    if (!f) return -1;
+
+    int capacity = 10;
+    int count = 0;
+
+    Edge *e = malloc(capacity * sizeof(Edge));
+
+    while (fscanf(f, "%d %d", &e[count].u, &e[count].v) == 2)
+    {
+        count++;
+
+        if (count >= capacity)
+        {
+            capacity *= 2;
+            e = realloc(e, capacity * sizeof(Edge));
+        }
+    }
+
+    fclose(f);
+    *edges = e;
     return count;
 }
+
